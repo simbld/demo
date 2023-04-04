548f81a6dd677e8f714c955dc383a16a2a43277e
Simon Bullado <s_bullado51@hotmail.com>
2023-04-04 14:51:47 +0200

    Add PostMapping in BookController

--- src/main/java/com/udemy/demo/book/BookController.java
+++ src/main/java/com/udemy/demo/book/BookController.java
@@ -3,10 +3,12 @@ package com.udemy.demo.book; // Déclaration du package contenant la classe
 import org.springframework.http.HttpStatus; // Importation de la classe HttpStatus du package org.springframework.http
 import org.springframework.http.ResponseEntity; // Importation de la classe ResponseEntity du package org.springframework.http
 import org.springframework.web.bind.annotation.GetMapping; // Importation de la classe GetMapping du package org.springframework.web.bind.annotation
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController; // Importation de la classe RestController du package org.springframework.web.bind.annotation
 
 import java.util.Arrays; // Importation de la classe Arrays du package java.util
 
+// Creation des web services avec Spring MVC
 @RestController // permet de déclarer un bean Spring qui gère les requêtes HTTP et renvoie des réponses HTTP et qui permet de l'exposer au monde extérieur
 public class BookController { // Creation du endpoint "/books" qui permet d'afficher les livres disponibles
 
@@ -28,7 +30,11 @@ public class BookController { // Creation du endpoint "/books" qui permet d'affi
 
         return new ResponseEntity(Arrays.asList(book1, book2, book3), HttpStatus.OK); /* La méthode java Arrays.asList retourne a Spring MVC un new objet de type "ResponseEntity" dans lequel on va stocker la liste de livres et
         qui permet de retourner une réponse HTTP personnalisée avec un code de statut et des en-têtes HTTP.
-        la méthode retourne une liste de livres encapsulée dans un objet new "ResponseEntity", avec un code d'état HTTP "200 OK"
-        @ResponseBody  (la valeur de retour est sérialisée en JSON) + @RequestBody (le corps de la requête est désérialisé en objet Java)*/
+        la méthode retourne une liste de livres encapsulée dans un objet new "ResponseEntity" // la méthode retourne un code d'état HTTP "200 OK" */
+    }
+    @PostMapping(value = "/books") /* est une annotation Spring qui est utilisée pour mapper la méthode addBook() à l'URL "/books". Cela signifie que lorsque l'application reçoit une requête HTTP POST à cette URL,
+    la méthode addBook() sera exécutée et renverra une réponse HTTP contenant les informations demandées. */
+    public ResponseEntity addBook(Book book) {
+        return new ResponseEntity<>(book, HttpStatus.CREATED); // la méthode retourne un code d'état HTTP "201 CREATED"
     }
 }
