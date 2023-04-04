325e75adc2ea7e4181733742d8520a2b1f5b767a
Simon Bullado <s_bullado51@hotmail.com>
2023-04-04 12:59:19 +0200

    Add comment Book

--- src/main/java/com/udemy/demo/book/BookController.java
+++ src/main/java/com/udemy/demo/book/BookController.java
@@ -1,21 +1,33 @@
-package com.udemy.demo.book;
+package com.udemy.demo.book; // Déclaration du package contenant la classe
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus; // Importation de la classe HttpStatus du package org.springframework.http
+import org.springframework.http.ResponseEntity; // Importation de la classe ResponseEntity du package org.springframework.http
+import org.springframework.web.bind.annotation.GetMapping; // Importation de la classe GetMapping du package org.springframework.web.bind.annotation
+import org.springframework.web.bind.annotation.RestController; // Importation de la classe RestController du package org.springframework.web.bind.annotation
 
-import java.util.Arrays;
+import java.util.Arrays; // Importation de la classe Arrays du package java.util
 
-@RestController
-public class BookController {
+@RestController // @Controller + @ResponseBody (la valeur de retour est sérialisée en JSON) + @RequestBody (le corps de la requête est désérialisé en objet Java)
+public class BookController { // @Controller + @ResponseBody (la valeur de retour est sérialisée en JSON) + @RequestBody (le corps de la requête est désérialisé en objet Java)
 
-    @GetMapping(value="/books")
-    public ResponseEntity listBooks() {
-        Book book = new Book();
-        book.setTitle("MyBook");
-        book.setCategory(new Category("BD"));
+    @GetMapping(value="/books") // est une annotation Spring qui est utilisée pour mapper la méthode listBooks() à l'URL "/books". Cela signifie que lorsque l'application reçoit une requête HTTP GET à cette URL,
+    // la méthode listBooks() sera exécutée et renverra une réponse HTTP contenant les informations demandées. // @RequestMapping(value="/books", method=RequestMethod.GET) + @ResponseBody (la valeur de retour est sérialisée en JSON)
+    public ResponseEntity listBooks() { // méthode "listBooks" qui est définie dans la classe "BookController" utilise l'annotation "@GetMapping" qui indique que cette méthode répond à une requête HTTP GET sur le chemin "/books"
+        // (la valeur de retour est sérialisée en JSON) + @RequestBody (le corps de la requête est désérialisé en objet Java)
+        Book book1 = new Book(); // Créer le premier livre // @RequestBody  (le corps de la requête est désérialisé en objet Java)
+        book1.setTitle("MyBook1"); // Définir le titre du premier livre
+        book1.setCategory(new Category("BD")); // Définir la catégorie du premier livre
 
-        return new ResponseEntity(Arrays.asList(book), HttpStatus.OK);
+        Book book2 = new Book();
+        book2.setTitle("MyBook2");
+        book2.setCategory(new Category("Roman"));
+
+        Book book3 = new Book();
+        book3.setTitle("MyBook3");
+        book3.setCategory(new Category("Science"));
+
+        return new ResponseEntity(Arrays.asList(book1, book2, book3), HttpStatus.OK); // La méthode retourne un objet de type "ResponseEntity" qui permet de retourner une réponse HTTP personnalisée avec un code de statut et des en-têtes HTTP.
+        //la méthode retourne une liste de livres encapsulée dans un objet "ResponseEntity", avec un code d'état HTTP "200 OK"
+        // @ResponseBody  (la valeur de retour est sérialisée en JSON) + @RequestBody (le corps de la requête est désérialisé en objet Java)
     }
 }
