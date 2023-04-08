d6936e34d80e512e8206733223199945ef7b236e
Simon Bullado <s_bullado51@hotmail.com>
2023-04-08 16:37:21 +0200

    Add Post and Delete in BorrowController

--- src/main/java/com/udemy/demo/borrow/BorrowController.java
+++ src/main/java/com/udemy/demo/borrow/BorrowController.java
@@ -2,29 +2,29 @@ package com.udemy.demo.borrow;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 
 @RestController
 public class BorrowController {
+
     @GetMapping("/borrows")
     public ResponseEntity getMyBorrows() {
-        //TODO:
+
         Borrow borrow = new Borrow();
         borrow.setAskDate(LocalDate.now());
-        return new ResponseEntity(borrow, HttpStatus.OK);
+        return new ResponseEntity<>(borrow, HttpStatus.OK);
     }
-    @PostMapping("/borrows/{bookId}")
+
+    @PostMapping(value = "/borrows/{bookId}")
     public ResponseEntity createBorrow(@PathVariable("bookId") String bookId) {
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
-    @DeleteMapping("/borrows/{borrowId}")
+    @DeleteMapping(value = "/borrows/{borrowId}")
     public ResponseEntity deleteBorrow(@PathVariable("borrowId") String borrowId) {
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
