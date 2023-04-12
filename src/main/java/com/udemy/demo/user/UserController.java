9b0c54cd534e99dc2e9730f537991adf1c8798d7
Simon Bullado <s_bullado51@hotmail.com>
2023-04-12 07:32:54 +0200

    Add @Valid and @RequestBody which validates the data provided in the body of the HTTP request

--- src/main/java/com/udemy/demo/user/UserController.java
+++ src/main/java/com/udemy/demo/user/UserController.java
@@ -1,5 +1,6 @@
 package com.udemy.demo.user;
 
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,11 +12,13 @@ import java.util.List;
 
 @RestController
 public class UserController {
-    private List<User> userList = new ArrayList<>(); // Implémentation des méthodes CRUD pour les utilisateurs en utilisant userList
 
     @PostMapping(value = "/users")
-    public ResponseEntity<User> addUser(@RequestBody User user) {
-        userList.add(user);
-        return new ResponseEntity<>(user, HttpStatus.CREATED);
+    public ResponseEntity addUser(
+            @Valid
+            @RequestBody User user) {
+
+        User userOld = new User("toto@email.com");
+        return new ResponseEntity(userOld, HttpStatus.CREATED);
     }
 }
