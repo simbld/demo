b2965535a359ce2ef55f621f031b006e9cb2cc92
Simon Bullado <s_bullado51@hotmail.com>
2023-04-18 18:25:21 +0200

    transformation of Category class into JPA spring data entity

--- src/main/java/com/udemy/demo/book/Category.java
+++ src/main/java/com/udemy/demo/book/Category.java
@@ -1,9 +1,28 @@
 package com.udemy.demo.book;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
     private String label;
 
     public Category() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public Category(String label) {
