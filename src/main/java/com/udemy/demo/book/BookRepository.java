e82107cf801eb0ee268393e4b12a5300c6df621e
simbld <s_bullado51@hotmail.com>
2023-05-10 18:33:03 +0200

    creation of the spring data jpa method which allows to retrieve the available books

---
+++ src/main/java/com/udemy/demo/book/BookRepository.java
@@ -0,0 +1,12 @@
+package com.udemy.demo.book;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BookRepository extends CrudRepository<Book, Integer> {
+
+    List<Book> findByStatusAndUserIdNotAndDeletedFalse (BookStatus bookStatus, Integer userId);
+}
